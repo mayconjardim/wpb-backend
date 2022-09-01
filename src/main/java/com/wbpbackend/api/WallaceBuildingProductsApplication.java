ef0fef3bdfb2db7df450b92e66bf53116d8362c2
Maycon Jardim <mayconsmill@gmail.com>
2022-09-01 20:51:45 -0300

    Projeto criado

---
+++ src/main/java/com/wbpbackend/api/WallaceBuildingProductsApplication.java
@@ -0,0 +1,13 @@
+package com.wbpbackend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WallaceBuildingProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WallaceBuildingProductsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbpbackend/api/WallaceBuildingProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbpbackend.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WallaceBuildingProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
