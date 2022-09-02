507e61e890115fb63b1eeffcf3def16154fa15bb
Maycon Jardim <mayconsmill@gmail.com>
2022-09-01 21:24:24 -0300

    Entidade abstrata Person

---
+++ src/main/java/com/wbpbackend/api/entities/Dispatcher.java
@@ -0,0 +1,11 @@
+package com.wbpbackend.api.entities;
+
+import java.io.Serializable;
+
+public class Dispatcher extends Person implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	
+	
+}
+
