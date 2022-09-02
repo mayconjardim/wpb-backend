fab95aa7514607d6eae8c3b40e0d8a68029e9bfb
Maycon Jardim <mayconsmill@gmail.com>
2022-09-01 21:41:59 -0300

    Entidade manager

--- src/main/java/com/wbpbackend/api/entities/Manager.java
+++ src/main/java/com/wbpbackend/api/entities/Manager.java
@@ -1,5 +1,38 @@
 package com.wbpbackend.api.entities;
 
-public class Manager {
+import java.io.Serializable;
 
+public class Manager extends Person implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String phoneNumber;
+	private String email;
+	
+	public Manager() {
+		super();
+	}
+
+	public Manager(Long id, String name, String phoneNumber, String email) {
+		super(id, name);
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
 }
