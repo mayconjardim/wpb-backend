900cb9f52bed73a1b3ff393f793cb3a1a14bd888
Maycon Jardim <mayconsmill@gmail.com>
2022-09-01 22:04:41 -0300

    Priority e Status enums

---
+++ src/main/java/com/wbpbackend/api/enums/Priority.java
@@ -0,0 +1,36 @@
+package com.wbpbackend.api.enums;
+
+public enum Priority {
+
+	LOW(0, "LOW"), MEDIUM(1, "MEDIUM"), HIGH(2, "HIGH");
+
+	private Integer cod;
+	private String description;
+
+	private Priority(Integer cod, String description) {
+		this.cod = cod;
+		this.description = description;
+	}
+
+	public Integer getCod() {
+		return cod;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+	
+	public static Priority toEnum(Integer cod) {
+		if (cod == null) {
+			return null;
+		}
+		
+		for (Priority x : Priority.values()) {
+			if (cod.equals(x.getCod())) {
+				return x;
+			}
+		}
+		throw new IllegalArgumentException("Invalid Priority: " + cod);
+	}
+
+}
