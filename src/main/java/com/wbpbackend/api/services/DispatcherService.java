067dfd2dc9fa04e8a021a3006970453b77e62892
Maycon Jardim <mayconsmill@gmail.com>
2022-09-04 14:27:02 -0300

    Atulizando Dispatcher

--- src/main/java/com/wbpbackend/api/services/DispatcherService.java
+++ src/main/java/com/wbpbackend/api/services/DispatcherService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,4 +42,16 @@ public class DispatcherService {
 		return new DispatcherDTO(entity);
 	}
 
+	@Transactional
+	public DispatcherDTO update(Long id, DispatcherDTO dto) {
+		try {
+			Dispatcher entity  = dispatcherRepository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = dispatcherRepository.save(entity);
+			return new DispatcherDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ObjectNotFoundException("Id not found " + id);
+		}
+	}
 }
